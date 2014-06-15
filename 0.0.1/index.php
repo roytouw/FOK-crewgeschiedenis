a14d4ef613f18ff07c813f4f5be2f5ab50c5364b
zarGonnn <yavuzselim@gmail.com>
2014-06-15 04:41:50 +0200

    Index.php opgeschoond.

--- 0.0.1/index.php
+++ 0.0.1/index.php
@@ -1,9 +1,3 @@
-<?php
-define("_BBC_PAGE_NAME", $_SERVER['REQUEST_URI']);
-define("_BBCLONE_DIR", "../bbclone/");
-define("COUNTER", _BBCLONE_DIR."mark_page.php");
-if (is_readable(COUNTER)) include_once(COUNTER);
-?>
 <html>
 <head>
   <!--Load the AJAX API-->
